3d6fec5ac381e55306fbb1912a606b79bbe7cb02
cibernox <miguel.camba@gmail.com>
2017-01-29 11:09:01 +0000

    Allow deep merge. Also:
    - Use switch to ESLint
    - Update Ember and Ember-cli
    - Improve package.json

--- addon/helpers/assign.js
+++ addon/helpers/assign.js
@@ -1,9 +1,14 @@
 import Ember from 'ember';
+import merge from 'lodash/merge';
 
 const eAssign = Ember.assign || Object.assign;
 
-export function assign(params) {
-  return eAssign({}, ...params);
+export function assign(params, { deep = false } = {}) {
+  if (deep) {
+    return merge({}, ...params);
+  } else {
+    return eAssign({}, ...params);
+  }
 }
 
 export default Ember.Helper.helper(assign);
--- tests/unit/helpers/assign-test.js
+++ tests/unit/helpers/assign-test.js
@@ -6,8 +6,43 @@ module('Unit | Helper | assign');
 test('It is equivalent to doing `Object.assign(...params)`', function(assert) {
   let params = [{ foo: 'bar', bar: 'baz' }, { bar: 'foobar', qux: 'quux' }];
   let result = assign(params);
-  assert.equal(result.foo, 'bar');
-  assert.equal(result.bar, 'foobar');
-  assert.equal(result.qux, 'quux');
+  assert.deepEqual(result, {
+    foo: 'bar',
+    bar: 'foobar',
+    qux: 'quux'
+  });
+});
+
+
+test('If passed `deep=true` it performs a deep merge', function(assert) {
+  let params = [
+    {
+      foo: 'bar',
+      bar: 'baz',
+      settings: {
+        quotes: 'double',
+        parens: 'always'
+      }
+    },
+    {
+      bar: 'foobar',
+      qux: 'quux',
+      settings: {
+        quotes: 'single',
+        spaces: 'around'
+      }
+    }
+  ];
+  let result = assign(params, { deep: true });
+  assert.deepEqual(result, {
+    foo: 'bar',
+    bar: 'foobar',
+    qux: 'quux',
+    settings: {
+      quotes: 'single',
+      parens: 'always',
+      spaces: 'around'
+    }
+  })
 });
 
