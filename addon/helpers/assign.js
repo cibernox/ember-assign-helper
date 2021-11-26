0028a3ff1bbc55c12b4ae73df3faf6b3b0298b2a
Miguel Camba <miguel.camba@gmail.com>
2021-11-26 22:12:20 +0100

    0.4.0

--- addon/helpers/assign.js
+++ addon/helpers/assign.js
@@ -1,8 +1,7 @@
-import { assign as oAssign } from '@ember/polyfills'
 import { helper as buildHelper } from "@ember/component/helper";
 
 export function assign(params) {
-  return oAssign({}, ...params);
+  return Object.assign({}, ...params);
 }
 
 export default buildHelper(assign);
