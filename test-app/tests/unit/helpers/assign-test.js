c0108f2de25d26a2e696727e9c839a15bbc3dea3
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-01-08 09:51:02 +0100

    Fix lint

--- test-app/tests/unit/helpers/assign-test.js
+++ test-app/tests/unit/helpers/assign-test.js
@@ -3,11 +3,13 @@ import { module, test } from 'qunit';
 
 module('Unit | Helper | assign');
 
-test('It is equivalent to doing `Object.assign(...params)`', function(assert) {
-  let params = [{ foo: 'bar', bar: 'baz' }, { bar: 'foobar', qux: 'quux' }];
+test('It is equivalent to doing `Object.assign(...params)`', function (assert) {
+  let params = [
+    { foo: 'bar', bar: 'baz' },
+    { bar: 'foobar', qux: 'quux' },
+  ];
   let result = assign(params);
-  assert.equal(result.foo, 'bar');
-  assert.equal(result.bar, 'foobar');
-  assert.equal(result.qux, 'quux');
+  assert.strictEqual(result.foo, 'bar');
+  assert.strictEqual(result.bar, 'foobar');
+  assert.strictEqual(result.qux, 'quux');
 });
-
